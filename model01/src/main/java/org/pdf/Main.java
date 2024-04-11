f14c14b44c0d97eaff75fce11f9759cc95ad0420
akai <62873177+akai-china@users.noreply.github.com>
2024-04-11 14:44:52 +0800

    squash! hello2

--- model01/src/main/java/org/pdf/Main.java
+++ model01/src/main/java/org/pdf/Main.java
@@ -2,6 +2,7 @@ package org.pdf;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("nihao ");
         System.out.println("nihao");
         System.out.println("Hello world!");
     }
