c60492861154af275410583f8cfa2214059b5f96
thinkpad_mk <thinkpad_mk@users.noreply.github.com>
2024-04-17 17:44:14 +0800

    main3最后加回车

---
+++ model01/src/main/java/org/pdf/Main3.java
@@ -0,0 +1,9 @@
+package org.pdf;
+
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("nihao ");
+        System.out.println("nihao");
+        System.out.println("Hello world!");
+    }
+}
