883de78f8f93856d0e1eb261bbdeaeb38ea65726
thinkpad_mk <thinkpad_mk@users.noreply.github.com>
2024-04-17 17:40:05 +0800

    main2最后不加回车

---
+++ model01/src/main/java/org/pdf/Main2.java
@@ -0,0 +1,9 @@
+package org.pdf;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("nihao ");
+        System.out.println("nihao");
+        System.out.println("Hello world!");
+    }
+}
