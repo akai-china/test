4e3653eaee53b11879f3867ce16b6092d93ccc01
thinkpad_mk <thinkpad_mk@users.noreply.github.com>
2024-04-22 14:04:00 +0800

    squash! hello2

--- model01/src/main/java/org/pdf/Main.java
+++
@@ -1,9 +0,0 @@
-package org.pdf;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("nihao ");
-        System.out.println("nihao");
-        System.out.println("Hello world!");
-    }
-}
