4105d1f791d5d0b0c8cb8786defdd57df9c28258
thinkpad_mk <thinkpad_mk@users.noreply.github.com>
2024-04-22 14:42:31 +0800

    squash! hello2

--- model01/src/main/java/org/pdf/Main3.java
+++ model01/src/main/java/org/pdf/Main3.java
@@ -2,11 +2,11 @@ package org.pdf;
 
 /**
  * Auther mk
- * Date 2024/4/22 下午2:11
+ * Date 2024/4/22 下午2:12
  * Description:
  */
 public class Main3 {
     public static void main(String[] args) {
-        System.out.println("我是main");
+        System.out.println("我是真mainewqe");
     }
 }
